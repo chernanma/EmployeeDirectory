ad5f1df6a5ba9aadc8c0a6ed3ff32bf6bc6aa97a
Cesar Martinez <chernanma@msn.com>
2021-01-14 22:16:57 -0500

    Adding Style to CardEmployees

--- employeedirectory/src/components/CardEmployee/index.js
+++ employeedirectory/src/components/CardEmployee/index.js
@@ -4,21 +4,22 @@ import Row from "../Row";
 import Col from "../Col";
 
 function CardEmployee(props) {
-  const width={ width: "100%" };
+  const style={ width: "100%", height: "auto" };
+  
   return (
-    <div className="card" style={width}>
-      <div className="card-header"><h1>{props.firstname} {props.lastname}</h1></div>
+    <div className="card" style={style}>
+      <div className="card-header"><h3>{props.firstname} {props.lastname}</h3></div>
       <div className="card-body">
         <Row>
-          <Col size="md-9">
-            <h4>Phone: {props.phone}</h4>
-            <h4>Email: {props.email}</h4>
-            <h4>Street: {props.housenumber} {props.streetname}</h4>
-            <h4>City: {props.city}</h4> <h4> Zip Code: {props.zipcode} </h4>
-            <h4>State: {props.state}</h4>            
-            <h4>Years Working: {props.age}</h4>
+          <Col size="md-4">            
+            <span><h5>Email: </h5>{props.email}</span>            
+            <span><h5>Phone: </h5>{props.phone}</span>            
           </Col>   
-          <Col size="md-3">
+          <Col size="md-4">
+            <span><h5>Address: </h5> {props.housenumber} {props.streetname}, {props.city}, {props.state} {props.zipcode}</span>                
+            <span><h5>Years Working: </h5>{props.age}</span>
+          </Col>
+          <Col size="md-4">
             <img alt="Pic" src={props.image} className="img-fluid" />
           </Col>
         </Row>        
